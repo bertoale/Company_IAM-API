3083d8479e16054efbe18290a584e71b2ca4c993
bertoale <albertdarma@gmail.com>
2026-02-11 17:19:33 +0800

    update routes

--- internal/role_permission/route.go
+++ internal/role_permission/route.go
@@ -10,7 +10,14 @@ func SetupRoutes(r *gin.Engine, ctrl *Controller, cfg *config.Config) {
 	routeGroup := r.Group("/api/role-permissions")
 	{
 		routeGroup.POST("/", ctrl.CreateRolePermission)
-		routeGroup.DELETE("/:id", ctrl.DeleteRolePermission)
+		routeGroup.DELETE(
+			"/roles/:roleID/permissions/:permissionID",
+
+			ctrl.DeleteRolePermission,
+		)
+		routeGroup.GET("/roles/:id", ctrl.FindByRoleID)
+		routeGroup.GET("/permissions/:id", ctrl.FindByPermissionID)
+		routeGroup.GET("/roles/:id/permissions", ctrl.FindByRoleIDWithPermission)
 
 	}
 }
